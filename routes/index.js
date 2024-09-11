43adc84efae3c12bc24426dad94b635a0a463075
b41r4m <balramas11@gmail.com>
2024-09-11 20:44:15 +0530

    done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const Movie = require("../models/Movie.model");
+
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+// GET movies page
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then((allTheMovieFromDB) => {
+      console.log("Retrieved movies from DB:", allTheMovieFromDB);
+      res.render("movies", { movies: allTheMovieFromDB });
+    })
+    .catch((error) => {
+      console.log("Error while getting the movies from the DB: ", error);
+
+      next(error);
+    });
+});
+
+//GET movie details page
+router.get("/movies/:movieId", (req, res, next) => {
+  const { movieId } = req.params;
+
+  Movie.findById(movieId)
+    .then((theMovie) => res.render("details", { movie: theMovie }))
+    .catch((error) => {
+      console.log("Error while retrieving book details: ", error);
+
+      next(error);
+    });
+});
 
 module.exports = router;
